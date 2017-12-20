3bce90476539fb29e1577841006605e2d52e3c42
Daniel Fallon <dfallon@dvtrading.co>
2017-12-19 20:46:17 -0600

    General Code Cleanup
     - reformatting
     - removing magic numbers

--- core/src/main/scala/janstenpickle/vault/core/VaultConfig.scala
+++ core/src/main/scala/janstenpickle/vault/core/VaultConfig.scala
@@ -1,6 +1,6 @@
 package janstenpickle.vault.core
 
-import java.net.URL
+import java.net.{URL, HttpURLConnection => status}
 import java.nio.charset.Charset
 
 import dispatch.{Req, url}
@@ -14,37 +14,35 @@ import uscala.concurrent.result.AsyncResult
 import scala.concurrent.ExecutionContext
 
 case class VaultConfig(wsClient: WSClient, token: AsyncResult[String, String])
+
 @deprecated("Vault 0.6.5 deprecated AppId in favor of AppRole", "0.4.0")
 case class AppId(app_id: String, user_id: String)
+
 case class AppRole(role_id: String, secret_id: String)
 
 object VaultConfig {
 
   @deprecated("Vault 0.6.5 deprecated AppId in favor of AppRole", "0.4.0")
   def apply(client: WSClient, appId: AppId)
-  (implicit ec: ExecutionContext): VaultConfig =
+           (implicit ec: ExecutionContext): VaultConfig =
     VaultConfig(client,
-      client.path("auth/app-id/login").
-      post(appId.asJson).
-      toAsyncResult.
-      // scalastyle:off magic.number
-      acceptStatusCodes(200).
-      // scalastyle:on magic.number
-      extractFromJson[String](
+      client.path("auth/app-id/login")
+        .post(appId.asJson)
+        .toAsyncResult
+        .acceptStatusCodes(status.HTTP_OK)
+        .extractFromJson[String](
         _.downField("auth").downField("client_token")
       )
     )
 
   def apply(client: WSClient, appRole: AppRole)
-  (implicit ec: ExecutionContext): VaultConfig =
+           (implicit ec: ExecutionContext): VaultConfig =
     VaultConfig(client,
-      client.path("auth/approle/login").
-      post(appRole.asJson).
-      toAsyncResult.
-      // scalastyle:off magic.number
-      acceptStatusCodes(200).
-      // scalastyle:on magic.number
-      extractFromJson[String](
+      client.path("auth/approle/login")
+        .post(appRole.asJson)
+        .toAsyncResult
+        .acceptStatusCodes(status.HTTP_OK)
+        .extractFromJson[String](
         _.downField("auth").downField("client_token")
       )
     )
@@ -54,12 +52,11 @@ object VaultConfig {
 }
 
 
-
 case class WSClient(server: URL,
                     version: String = "v1") {
-   def path(p: String): Req =
-     url(s"${server.toString}/$version/$p")
-       .setContentType("application/json", Charset.forName("UTF-8"))
+  def path(p: String): Req =
+    url(s"${server.toString}/$version/$p")
+      .setContentType("application/json", Charset.forName("UTF-8"))
 }
 
 
