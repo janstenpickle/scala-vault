27b72b6fb2505ba51d42d9f6b3f80c3168aead8a
Lorand Szakacs <lsz@lorandszakacs.com>
2017-12-21 14:31:58 +0200

    Update tests in core

--- core/src/it/scala/janstenpickle/vault/core/SecretsIT.scala
+++ core/src/it/scala/janstenpickle/vault/core/SecretsIT.scala
@@ -1,84 +1,85 @@
-//package janstenpickle.vault.core
-//
-//import org.scalacheck.Prop
-//import org.specs2.ScalaCheck
-//
-//class GenericIT extends SecretsTests {
-//  override def backend: String = "secret"
-//}
-//
-//class CubbyHoleIT extends SecretsTests {
-//  override def backend: String = "cubbyhole"
-//}
-//
-//trait SecretsTests extends VaultSpec with ScalaCheck {
-//  import VaultSpec._
-//
-//  override def is =
-//    s2"""
-//      Can set a secret in vault $set
-//      Can set and get a secret in vault $get
-//      Can list keys $list
-//      Can set multiple subkeys $setMulti
-//      Can set and get multiple subKeys $getSetMulti
-//      Cannot get non-existent key $failGet
-//      Fails to perform actions on a non-vault server $failSetBadServer
-//      Fails to perform actions with a bad token $failSetBadToken
-//      """
-//
-//  def backend: String
-//
-//  lazy val good = Secrets(config, backend)
-//  lazy val badToken = Secrets(badTokenConfig, backend)
-//  lazy val badServer = Secrets(badServerConfig, backend)
-//
-//  def set = Prop.forAllNoShrink(strGen, strGen) { (key, value) =>
-//    good.set(key, value).attemptRun(_.getMessage()) must beOk
-//  }
-//
-//  def get = Prop.forAllNoShrink(strGen, strGen) { (key, value) =>
-//    (good.set(key, value).attemptRun(_.getMessage()) must beOk) and
-//    (good.get(key).attemptRun(_.getMessage()) must beOk.like {
-//      case a => a === value
-//    })
-//  }
-//
-//  def list = Prop.forAllNoShrink(strGen, strGen, strGen) { (key1, key2, value) =>
-//    (good.set(key1, value).attemptRun(_.getMessage()) must beOk) and
-//    (good.set(key2, value).attemptRun(_.getMessage()) must beOk) and
-//    (good.list.attemptRun(_.getMessage()) must beOk[List[String]].like {
-//      case a => a must containAllOf(Seq(key1, key2))
-//    })
-//  }
-//
-//  def setMulti = Prop.forAllNoShrink(strGen, strGen, strGen, strGen) {
-//    (key1, key2, value1, value2) =>
-//    good.set(
-//      "nicolas-cage",
-//      Map(key1 -> value1, key2 -> value2)
-//    ).attemptRun(_.getMessage()) must beOk
-//  }
-//
-//  def getSetMulti = Prop.forAllNoShrink(
-//    strGen, strGen, strGen, strGen, strGen
-//  ) { (key1, key2, value1, value2, mainKey) =>
-//    val testData = Map(key1 -> value1, key2 -> value2)
-//    (good.set(mainKey, testData).attemptRun(_.getMessage()) must beOk) and
-//    (good.getAll(mainKey).attemptRun(_.getMessage()) must beOk.like {
-//      case a => a === testData
-//    })
-//  }
-//
-//  def failGet = good.get("john").attemptRun(_.getMessage()) must beFail.
-//    like { case err =>
-//      err must contain("Received failure response from server: 404")
-//    }
-//
-//  def failSetBadServer = badServer.set(
-//    "nic", "cage"
-//  ).attemptRun(_.getMessage()) must beFail
-//
-//  def failSetBadToken = badToken.set(
-//    "nic", "cage"
-//  ).attemptRun(_.getMessage()) must beFail
-//}
+package janstenpickle.vault.core
+
+import janstenpickle.scala.result._
+import org.scalacheck.Prop
+import org.specs2.ScalaCheck
+
+class GenericIT extends SecretsTests {
+  override def backend: String = "secret"
+}
+
+class CubbyHoleIT extends SecretsTests {
+  override def backend: String = "cubbyhole"
+}
+
+trait SecretsTests extends VaultSpec with ScalaCheck {
+  import VaultSpec._
+
+  override def is =
+    s2"""
+      Can set a secret in vault $set
+      Can set and get a secret in vault $get
+      Can list keys $list
+      Can set multiple subkeys $setMulti
+      Can set and get multiple subKeys $getSetMulti
+      Cannot get non-existent key $failGet
+      Fails to perform actions on a non-vault server $failSetBadServer
+      Fails to perform actions with a bad token $failSetBadToken
+      """
+
+  def backend: String
+
+  lazy val good = Secrets(config, backend)
+  lazy val badToken = Secrets(badTokenConfig, backend)
+  lazy val badServer = Secrets(badServerConfig, backend)
+
+  def set = Prop.forAllNoShrink(strGen, strGen) { (key, value) =>
+    good.set(key, value).attemptRun must beRight
+  }
+
+  def get = Prop.forAllNoShrink(strGen, strGen) { (key, value) =>
+    (good.set(key, value).attemptRun must beRight) and
+    (good.get(key).attemptRun must beRight.like {
+      case a => a === value
+    })
+  }
+
+  def list = Prop.forAllNoShrink(strGen, strGen, strGen) { (key1, key2, value) =>
+    (good.set(key1, value).attemptRun must beRight) and
+    (good.set(key2, value).attemptRun must beRight) and
+    (good.list.attemptRun must beRight[List[String]].like {
+      case a => a must containAllOf(Seq(key1, key2))
+    })
+  }
+
+  def setMulti = Prop.forAllNoShrink(strGen, strGen, strGen, strGen) {
+    (key1, key2, value1, value2) =>
+    good.set(
+      "nicolas-cage",
+      Map(key1 -> value1, key2 -> value2)
+    ).attemptRun must beRight
+  }
+
+  def getSetMulti = Prop.forAllNoShrink(
+    strGen, strGen, strGen, strGen, strGen
+  ) { (key1, key2, value1, value2, mainKey) =>
+    val testData = Map(key1 -> value1, key2 -> value2)
+    (good.set(mainKey, testData).attemptRun must beRight) and
+    (good.getAll(mainKey).attemptRun must beRight.like {
+      case a => a === testData
+    })
+  }
+
+  def failGet = good.get("john").attemptRun must beLeft.
+    like { case err =>
+      err must contain("Received failure response from server: 404")
+    }
+
+  def failSetBadServer = badServer.set(
+    "nic", "cage"
+  ).attemptRun must beLeft
+
+  def failSetBadToken = badToken.set(
+    "nic", "cage"
+  ).attemptRun must beLeft
+}
--- core/src/it/scala/janstenpickle/vault/core/VaultSpec.scala
+++ core/src/it/scala/janstenpickle/vault/core/VaultSpec.scala
@@ -1,52 +1,54 @@
-//package janstenpickle.vault.core
-//
-//import java.net.URL
-//
-//import janstenpickle.scala.syntax.SyntaxRequest._
-//import janstenpickle.scala.syntax.ResponseSyntax._
-//import janstenpickle.scala.syntax.VaultConfigSyntax._
-//import org.scalacheck.Gen
-//import org.specs2.Specification
-//import org.specs2.specification.core.Fragments
-//
-//import scala.concurrent.ExecutionContext
-//import scala.io.Source
-//
-//
-//trait VaultSpec extends Specification with ResultMatchers {
-//  implicit val errConverter: Throwable => String = _.getMessage
-//  implicit val ec: ExecutionContext = ExecutionContext.global
-//
-//  lazy val rootToken = Source.fromFile("/tmp/.root-token").mkString.trim
-//  lazy val roleId = Source.fromFile("/tmp/.role-id").mkString.trim
-//  lazy val secretId = Source.fromFile("/tmp/.secret-id").mkString.trim
-//
-//  lazy val rootConfig: VaultConfig = VaultConfig(
-//    WSClient(new URL("http://localhost:8200")), rootToken
-//  )
-//  lazy val badTokenConfig = VaultConfig(
-//    rootConfig.wsClient,
-//    "face-off"
-//  )
-//  lazy val config = VaultConfig(
-//    rootConfig.wsClient,
-//    AppRole(roleId, secretId)
-//  )
-//  lazy val badServerConfig = VaultConfig(
-//    WSClient(new URL("http://nic-cage.xyz")),
-//    "con-air"
-//  )
-//
-////  def check = config.token.attemptRun(_.getMessage) must beOk
-//
-//  override def map(fs: => Fragments) =
-////    s2"""
-////      Can receive a token for an AppRole $check
-////    """ ^
-//    fs
-//}
-//
-//object VaultSpec {
-//  val longerStrGen = Gen.alphaStr.suchThat(_.length >= 3)
-//  val strGen = Gen.alphaStr.suchThat(_.nonEmpty)
-//}
+package janstenpickle.vault.core
+
+import java.net.URL
+
+import janstenpickle.scala.result._
+import janstenpickle.scala.syntax.SyntaxRequest._
+import janstenpickle.scala.syntax.ResponseSyntax._
+import janstenpickle.scala.syntax.VaultConfigSyntax._
+import org.scalacheck.Gen
+import org.specs2.Specification
+import org.specs2.matcher.EitherMatchers
+import org.specs2.specification.core.Fragments
+
+import scala.concurrent.ExecutionContext
+import scala.io.Source
+
+
+trait VaultSpec extends Specification with EitherMatchers {
+  implicit val errConverter: Throwable => String = _.getMessage
+  implicit val ec: ExecutionContext = ExecutionContext.global
+
+  lazy val rootToken: String = Source.fromFile("/tmp/.root-token").mkString.trim
+  lazy val roleId:    String = Source.fromFile("/tmp/.role-id").mkString.trim
+  lazy val secretId:  String = Source.fromFile("/tmp/.secret-id").mkString.trim
+
+  lazy val rootConfig: VaultConfig = VaultConfig(
+    WSClient(new URL("http://localhost:8200")), rootToken
+  )
+  lazy val badTokenConfig = VaultConfig(
+    rootConfig.wsClient,
+    "face-off"
+  )
+  lazy val config = VaultConfig(
+    rootConfig.wsClient,
+    AppRole(roleId, secretId)
+  )
+  lazy val badServerConfig = VaultConfig(
+    WSClient(new URL("http://nic-cage.xyz")),
+    "con-air"
+  )
+
+  def check = config.token.attemptRun should beRight
+
+  override def map(fs: => Fragments) =
+    s2"""
+      Can receive a token for an AppRole $check
+    """ ^
+    fs
+}
+
+object VaultSpec {
+  val longerStrGen = Gen.alphaStr.suchThat(_.length >= 3)
+  val strGen = Gen.alphaStr.suchThat(_.nonEmpty)
+}
